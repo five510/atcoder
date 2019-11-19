533a6cac8100005346ee2dedce19225b8b83bea6
five510 <pillochu5@egmail.com>
2019-11-19 15:58:31 +0900

    20191119

--- 20191116/main_c.py
+++ 20191116/main_c.py
@@ -1,16 +1,22 @@
+import itertools
 import math
 
 N = int(input())
-Xn = []
-Yn = []
-for _ in range(N):
-    x,y = map(int,input().split(' '))
-    Xn.append(x)
-    Yn.append(y)
+XY = [list(map(int,input().split(' '))) for _ in range(N)]
+All_combination = list(itertools.permutations(XY))
+All_distances = []
+for each_list in All_combination:
+    distance = 0
+    for i in range(1,N):
+        distance += math.sqrt((each_list[i-1][0] - each_list[i][0])**2 + (each_list[i-1][1] - each_list[i][1])**2) 
+    All_distances.append(distance)
+print(sum(All_distances)/len(All_distances))
 
+'''
 list_store = [[] * math.factorial(N)]
 
 tmp = range(n)
 def fanc(n_list):
     for val in n_list:
         Xn[i]
+'''
